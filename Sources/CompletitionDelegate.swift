f3deed1125d57764162bffebec19a67d0e347871
yespinoza <yespinoza@tecnosys.net>
2020-08-08 02:43:41 -0600

    Se agregan sources

---
+++ Sources/CompletitionDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  CompletitionDelegate.swift
+//  Posts
+//
+//  Created by yespinoza on 7/25/20.
+//
+
+import Foundation
+
+protocol CompletitionDelegate {
+    func didFinish(data:Data?)
+}
